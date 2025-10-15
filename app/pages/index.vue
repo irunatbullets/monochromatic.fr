ba2c98c10cd7201ee1807547eb15c4e40cfae995
irunatbullets <murray.nuttall@gmail.com>
2025-10-15 17:18:52 +0200

    ✨ Added nuxt-directus

---
+++ app/pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="h-screen flex justify-center items-center">
+    <img src="/monochromatic.svg" class="block w-1/4 min-w-66" alt="" />
+  </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { execSync } from 'child_process'
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: [
-    '@nuxt/content',
+    'nuxt-directus',    
     '@nuxt/eslint',
     '@nuxt/image',
     '@nuxt/fonts',
     '@unocss/nuxt'
   ],
   css: ['@unocss/reset/tailwind.css', '~/assets/css/main.css'],
+  app: {
+    baseURL: '',
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+    },
+  },
+  runtimeConfig: {
+    public: {
+      directus: {
+        url: 'https://content.monochromatic.fr/',
+      },
+      assetsUrl: 'https://content.monochromatic.fr/assets/',
+      lastPublished: execSync('git log -1 --format=%cd --date=iso')
+        .toString()
+        .trim(),
+    },
+  },
 })
